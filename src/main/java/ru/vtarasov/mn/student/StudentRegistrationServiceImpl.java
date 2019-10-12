91f0d84a832fc4b7474b8fa3f35245cd6b4ec8f0
Vladimir Tarasov <vtarasov@naumen.ru>
2019-10-13 01:47:49 +0500

    Logging was added

--- src/main/java/ru/vtarasov/mn/student/StudentRegistrationServiceImpl.java
+++ src/main/java/ru/vtarasov/mn/student/StudentRegistrationServiceImpl.java
@@ -3,11 +3,13 @@ package ru.vtarasov.mn.student;
 import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author vtarasov
  * @since 21.09.2019
  */
+@Slf4j
 @Singleton
 public class StudentRegistrationServiceImpl implements StudentRegistrationService {
     @Inject
@@ -15,16 +17,22 @@ public class StudentRegistrationServiceImpl implements StudentRegistrationServic
 
     @Override
     public Student register(Student student) {
-        return repository.save(student);
+        Student saved = repository.save(student);
+        LOG.info("Student was saved. Id: {}, Name: {}, Age: {}", student.getId(), student.getName(), student.getAge());
+        return saved;
     }
 
     @Override
     public void unregister(Student student) {
         repository.deleteById(student.getId());
+        LOG.info("Student was deleted. Id: {}, Name: {}, Age: {}", student.getId(), student.getName(), student.getAge());
     }
 
     @Override
     public Optional<Student> find(String id) {
-        return repository.findById(id);
+        Optional<Student> student = repository.findById(id);
+        student.ifPresent(student1 ->
+            LOG.info("Student was found. Id: {}, Name: {}, Age: {}", student1.getId(), student1.getName(), student1.getAge()));
+        return student;
     }
 }
